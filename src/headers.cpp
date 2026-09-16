bd3f86e9122065d0ed590ac0b92f81cb12899ffd
SerWike <fullname123@mail.ru>
2026-09-16 19:07:24 +0000

    Release functions for parse header

--- src/headers.cpp
+++ src/headers.cpp
@@ -1,21 +1,88 @@
 #include "headers.h"
 
+#include <algorithm>
 #include <ranges>
 #include <string_view>
+#include <utility>
 
 using namespace std::string_view_literals;
 
 using Callback = std::function<void(std::string_view, std::string_view)>;
 
+void iterHeaders(std::string_view req, Callback &&callback) {
+    auto is_space = [](char c) { return std::isspace(static_cast<unsigned char>(c)); };
+    auto trim = [&is_space](std::string_view s) {
+        if (s.empty())
+            return s;
+        auto first = std::ranges::find_if_not(s, is_space);
+        if (first == s.end())
+            first = s.begin();
 
-void iterHeaders(std::string_view req, Callback&& callback) {
-  // code here
+        auto last = std::ranges::find_if(first + 1, s.end(), is_space);
+
+        return std::string_view(first, last);
+    };
+
+    auto headers_start = req.find("\r\n");
+    if (headers_start == std::string_view::npos)
+        return;
+
+    auto headers_end = req.find("\r\n\r\n", headers_start);
+    if (headers_end == std::string_view::npos || headers_start == headers_end)
+        return;
+
+    for (const auto &line_subrange :
+         req.substr(headers_start + 2, headers_end - headers_start - 2) | std::views::split(std::string_view("\r\n"))) {
+        if (line_subrange.empty())
+            continue;
+        std::string_view line(line_subrange.begin(), line_subrange.end());
+
+        auto delim_pos = line.find(':');
+        if (delim_pos == std::string_view::npos)
+            continue;
+
+        std::string_view key = trim(line.substr(0, delim_pos));
+        std::string_view val = trim(line.substr(delim_pos + 1));
+
+        callback(key, val);
+    }
 }
 
 std::pair<std::string, std::string> findHostPort(std::string_view req) {
-  // code here
+    std::string_view host = "";
+    iterHeaders(req, [&host](std::string_view key, std::string_view val) {
+        if (key == "Host")
+            host = val;
+    });
+
+    if (host.empty())
+        throw std::runtime_error("Request without 'Host'");
+
+    auto delim = host.find(':');
+    if (delim == std::string_view::npos)
+        return std::make_pair(std::string(host), "80");
+
+    return std::make_pair(std::string(host.substr(0, delim)), std::string(host.substr(delim + 1)));
 }
 
 std::optional<size_t> findContentLength(std::string_view rsp) {
-  // code here
+    auto convert_to_number = [](std::string_view num) -> std::optional<size_t> {
+        if (num.empty())
+            return std::nullopt;
+
+        size_t value = 0;
+
+        auto [ptr, err] = std::from_chars(num.begin(), num.end(), value);
+        if (err == std::errc())
+            return value;
+        return std::nullopt;
+    };
+
+    std::string_view length = "";
+    iterHeaders(rsp, [&length](std::string_view key, std::string_view val) {
+        if (key == "Content-Length")
+            length = val;
+    });
+
+    return convert_to_number(length);
 }
--- tests/headers_test.cpp
+++ tests/headers_test.cpp
@@ -1,38 +1,164 @@
-#include <gtest/gtest.h>
 #include "headers.h"
+#include <gtest/gtest.h>
 
 TEST(iterHeaders, Empty) {
-  // code here
+    int count = 0;
+    ASSERT_NO_THROW(iterHeaders("", [&count](std::string_view key, std::string_view val) { ++count; }));
+
+    ASSERT_EQ(0, count);
 }
 
 TEST(iterHeaders, SkipRequestLine) {
-  // code here
+    std::string_view req = "GET /index.html HTTP/1.1\r\n"
+                           "Host: example.com\r\n"
+                           "\r\n";
+
+    int count = 0;
+    std::vector<std::pair<std::string_view, std::string_view>> headers;
+
+    iterHeaders(req, [&](std::string_view key, std::string_view val) {
+        ++count;
+        headers.emplace_back(key, val);
+    });
+
+    ASSERT_EQ(1, count);
+
+    ASSERT_EQ(1, headers.size());
+    EXPECT_EQ("Host", headers[0].first);
+    EXPECT_EQ("example.com", headers[0].second);
 }
 
 TEST(iterHeaders, SingleHeader) {
-  // code here
+    std::string_view req = "GET /index.html HTTP/1.1\r\n"
+                           "    Host    :   example.com   \r\n"
+                           "\r\n";
+
+    std::vector<std::pair<std::string_view, std::string_view>> headers;
+
+    iterHeaders(req, [&](std::string_view key, std::string_view val) { headers.emplace_back(key, val); });
+
+    ASSERT_EQ(1, headers.size());
+    EXPECT_EQ("Host", headers[0].first);
+    EXPECT_EQ("example.com", headers[0].second);
 }
 
 TEST(iterHeaders, MultipleHeaders) {
-  // code here
+    std::string_view req = "GET /index.html HTTP/1.1\r\n"
+                           "Host: example.com\r\n"
+                           "User-Agent: curl/8.0\r\n"
+                           "Accept: */*\r\n"
+                           "Content-Type: application/json\r\n"
+                           "Content-Length: 0\r\n"
+                           "\r\n";
+
+    std::vector<std::pair<std::string_view, std::string_view>> headers;
+
+    iterHeaders(req, [&](std::string_view key, std::string_view val) { headers.emplace_back(key, val); });
+
+    ASSERT_EQ(5, headers.size());
+
+    EXPECT_EQ("Host", headers[0].first);
+    EXPECT_EQ("example.com", headers[0].second);
+
+    EXPECT_EQ("User-Agent", headers[1].first);
+    EXPECT_EQ("curl/8.0", headers[1].second);
+
+    EXPECT_EQ("Accept", headers[2].first);
+    EXPECT_EQ("*/*", headers[2].second);
+
+    EXPECT_EQ("Content-Type", headers[3].first);
+    EXPECT_EQ("application/json", headers[3].second);
+
+    EXPECT_EQ("Content-Length", headers[4].first);
+    EXPECT_EQ("0", headers[4].second);
 }
 
 TEST(iterHeaders, MultipleSameHeaders) {
-  // code here
+    std::string_view req = "GET /index.html HTTP/1.1\r\n"
+                           "Host: example.com\r\n"
+                           "User-Agent: curl/8.0\r\n"
+                           "Accept: */*\r\n"
+                           "Content-Type: application/json\r\n"
+                           "Content-Length: 0\r\n"
+                           "Accept: *.zip\r\n"
+                           "\r\n";
+
+    std::map<std::string_view, std::string> headers;
+
+    iterHeaders(req, [&](std::string_view key, std::string_view val) {
+        if (!headers.contains(key))
+            headers[key] = "";
+        headers[key] = headers[key] + val;
+    });
+
+    ASSERT_EQ(5, headers.size());
+
+    ASSERT_TRUE(headers.contains("Host"));
+    EXPECT_EQ("example.com", headers["Host"]);
+
+    ASSERT_TRUE(headers.contains("User-Agent"));
+    EXPECT_EQ("curl/8.0", headers["User-Agent"]);
+
+    ASSERT_TRUE(headers.contains("Accept"));
+    EXPECT_EQ("*/**.zip", headers["Accept"]);
+
+    ASSERT_TRUE(headers.contains("Content-Type"));
+    EXPECT_EQ("application/json", headers["Content-Type"]);
+
+    ASSERT_TRUE(headers.contains("Content-Length"));
+    EXPECT_EQ("0", headers["Content-Length"]);
 }
 
 TEST(findHostPort, Simple) {
-  // code here
+    std::string_view req = "GET /index.html HTTP/1.1\r\n"
+                           "Host: example.com:85\r\n"
+                           "User-Agent: curl/8.0\r\n"
+                           "Accept: */*\r\n"
+                           "Content-Type: application/json\r\n"
+                           "Content-Length: 0\r\n"
+                           "\r\n";
+    std::pair<std::string, std::string> host_and_port;
+    ASSERT_NO_THROW(host_and_port = findHostPort(req));
+
+    ASSERT_EQ("example.com", host_and_port.first);
+    ASSERT_EQ("85", host_and_port.second);
 }
 
 TEST(findHostPort, NoHost) {
-  // code here
+    std::string_view req = "GET /index.html HTTP/1.1\r\n"
+                           "User-Agent: curl/8.0\r\n"
+                           "Accept: */*\r\n"
+                           "Content-Type: application/json\r\n"
+                           "Content-Length: 0\r\n"
+                           "\r\n";
+    std::pair<std::string, std::string> host_and_port;
+    ASSERT_THROW(host_and_port = findHostPort(req), std::runtime_error);
 }
 
 TEST(findContentLength, Simple) {
-  // code here
+    std::string_view req = "GET /index.html HTTP/1.1\r\n"
+                           "Host: example.com:85\r\n"
+                           "User-Agent: curl/8.0\r\n"
+                           "Accept: */*\r\n"
+                           "Content-Type: application/json\r\n"
+                           "Content-Length: 123\r\n"
+                           "\r\n";
+
+    auto content_length = findContentLength(req);
+
+    ASSERT_TRUE(content_length.has_value());
+    ASSERT_EQ(123, content_length.value());
 }
 
 TEST(findContentLength, NoContentLength) {
-  // code here
+    std::string_view req = "GET /index.html HTTP/1.1\r\n"
+                           "Host: example.com:85\r\n"
+                           "User-Agent: curl/8.0\r\n"
+                           "Accept: */*\r\n"
+                           "Content-Type: application/json\r\n"
+                           "\r\n";
+
+    auto content_length = findContentLength(req);
+
+    ASSERT_FALSE(content_length.has_value());
 }
